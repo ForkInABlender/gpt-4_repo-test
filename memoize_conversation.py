46a68793cf37e46ffcec10c708c27dd888598b8e
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-06-15 03:48:02 -0500

    Update memoize_conversation.py

--- memoize_conversation.py
+++ memoize_conversation.py
@@ -1,4 +1,25 @@
-#define the memoize_string function
+# Dylan Kenneth Eliot & GPT-3.5-turbo
+
+from base64 import b64encode as encoder, b64decode as decoder
+
+"""
+It takes a string in, encodes it as base64, then assigns it to a tuple in a list. The keys in memo are the parts of the string.
+
+To get the original string back, you need the dictionary that memoize_string gives you.
+
+
+GPT-4 did the work of writing and uploading it successfully.
+
+"""
+
+
+def dememoize_string(memo):
+    characters = []
+    for char, indices in memo.items():
+        for _, index in indices:
+            characters.append((index, char))
+    characters.sort()
+    return decoder(''.join(char for _, char in characters)).decode('utf-8')
 def memoize_string(string):
     memo = {}
     for index, char in enumerate(string):
@@ -6,12 +27,3 @@ def memoize_string(string):
             memo[char] = []
         memo[char].append((0, index))
     return memo
-
-# this is the conversation text
-conversation = \"\""
-User: what can zapier let you do?
-Assistant: Zapier is a powerful tool that allows you to automate tasks between different web apps. It's essentially a bridge that enables different applications to communicate and perform tasks without any manual intervention. Here are some of the things you can do with Zapier...\"\""
-
-# memoize the conversation
-memo = memoize_string(conversation)
-print(memo)
